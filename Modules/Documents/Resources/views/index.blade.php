62d809194db2a7939af0c37f25a8dfcb8a4de121
uvindu94 <uvinduanuradha94@gmail.com>
2024-09-10 16:24:38 +0530

    ui for add doc done

--- Modules/Documents/Resources/views/index.blade.php
+++ Modules/Documents/Resources/views/index.blade.php
@@ -12,38 +12,17 @@
                     {{ __("You're logged ins!") }}
                 </div>
 
+                {{-- this for normal user  --}}
                 <div class="row">
                     <div class="col-6 p-6">
-                        <h3>Add a new Document</h3>
 
-                        <section class="p-6">
-                        <form class="rounded-bottom">
-                            <div class="form-group">
-                            <label>Select Company</label>
-                            <select class="form-control" name="" id="">
-                                <option>company a</option>
-                                <option>company b</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group py-3">
-                          <label>Document Name</label>
-                         <input type="text" name="doc_name" id="doc_name" class="form-control" placeholder="Enter Document Name">
-                        </div>
-                        
-                        <div class="form-group py-2">
-                            <label>Upload Document (* pdf / wdx)</label>
-                            <input type="file" name="doc" id="doc" class="form-control">
-                        </div>
-
-                        <div class="form-group py-2">
-                            <input type="submit" class="btn btn-sm btn-primary">
-                        </div>
-                        </form>
-                    </section>
+                        @include('documents::user/addnewdoc')
 
                     </div>
-                    <div class="col-6">a</div>
+                    <div class="col-6">
+
+                        
+                    </div>
                 </div>
             </div>
         </div>
---
+++ Modules/Documents/Resources/views/user/addnewdoc.blade.php
@@ -0,0 +1,38 @@
+<h3>Add a new Document</h3>
+
+<section class="p-6">
+    <form class="rounded-bottom">
+        <div class="form-group">
+            <label>Select Company</label>
+            <select class="form-control" name="" id="">
+                <option>company a</option>
+                <option>company b</option>
+            </select>
+        </div>
+
+
+
+        <div class="form-group py-3">
+            <label>Document Name</label>
+            <input type="text" name="doc_name" id="doc_name" class="form-control"
+                placeholder="Enter Document Name">
+        </div>
+
+        <div class="form-group">
+            <label>Document Category</label>
+            <select class="form-control" name="" id="">
+                <option>category a</option>
+                <option>category b</option>
+            </select>
+        </div>
+
+        <div class="form-group py-2">
+            <label>Upload Document (* pdf / wdx)</label>
+            <input type="file" name="doc" id="doc" class="form-control">
+        </div>
+
+        <div class="form-group py-2">
+            <input type="submit" class="btn btn-sm btn-primary">
+        </div>
+    </form>
+</section>
